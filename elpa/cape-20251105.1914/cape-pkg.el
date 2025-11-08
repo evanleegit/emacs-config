c48a37c10b1120098055794c4be11fbc1c56aea0
evanleegit <evanstephenlee@gmail.com>
2025-11-08 08:58:07 -0800

    Initial commit of my Emacs config

---
+++ elpa/cape-20251105.1914/cape-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "cape" "20251105.1914"
+  "Completion At Point Extensions."
+  '((emacs  "29.1")
+    (compat "30"))
+  :url "https://github.com/minad/cape"
+  :commit "e0dd6eff88f690a2876bd98e5502ceee17530f1b"
+  :revdesc "e0dd6eff88f6"
+  :keywords '("abbrev" "convenience" "matching" "completion" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
